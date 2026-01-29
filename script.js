a41ccf0223df4cfb4387e6a8f40fb5e49af7943c
alok-404 <alokchandra2621@gmail.com>
2026-01-29 22:48:56 +0530

    Day 5: Productivity dashboard with real-time clock and todo logic

--- script.js
+++ script.js
@@ -42,4 +42,82 @@ realTime.innerHTML = ` <p class="label">Current Time</p>
          <h2>${hours}:${minutes}:${seconds} ${ampm}</h2>
         <span>${dayName}, ${day}, ${currentMonth}, ${year}</span>`;
 }
-setInterval(updateTime, 1000);
+setInterval(updateTime,1000)
+
+//to-do list section
+
+let todoInput = document.querySelector(".todoInput input");
+let todoInputAddBtn = document.querySelector(".todoInput button");
+let errorText = document.querySelector(".error-text");
+let todoList = document.querySelector(".todo-list");
+
+let allTodos = JSON.parse(localStorage.getItem("PDtodos")) || [];
+
+renderTodos()
+
+function LocalStorageSetItems(){
+  localStorage.setItem("PDtodos", JSON.stringify(allTodos));
+}
+
+todoInputAddBtn.addEventListener("click",function(){
+  let newTodo = todoInput.value.trim()
+  console.log(newTodo);
+  if(newTodo === ""){
+    errorText.style.display = "block";
+    return;
+  }
+
+  errorText.style.display = "none";
+
+  let todoObj = {
+    text:newTodo,
+    Id:Date.now(),
+    isCompleted:false
+  }
+  console.log(todoObj);
+
+  allTodos.push(todoObj)
+  LocalStorageSetItems();
+
+
+  createTodoElements(todoObj)
+})
+
+function createTodoElements(todoObj) {
+  const li = document.createElement("li");
+  li.dataset.id = todoObj.id;
+
+  if (todoObj.isCompleted) li.classList.add("done");
+
+  const label = document.createElement("label");
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = todoObj.isCompleted;
+
+  const span = document.createElement("span");
+  span.innerText = todoObj.text;
+
+  label.appendChild(checkbox);
+  label.appendChild(span);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.className = "delete";
+  deleteBtn.innerText = "✕";
+
+  li.appendChild(label);
+  li.appendChild(deleteBtn);
+  todoList.appendChild(li)
+
+  return li;
+}
+
+
+function renderTodos() {
+  todoList.innerHTML = ""; // clear first
+
+  allTodos.forEach(todo => {
+    const li = createTodoElements(todo);
+    todoList.appendChild(li);
+  });
+}
