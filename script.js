f94916fa3d3e8c246bb25eae541e80c3353ceb40
alok-404 <alokchandra2621@gmail.com>
2026-01-30 21:55:05 +0530

    Add README and update productivity dashboard logic
    :wq

--- script.js
+++ script.js
@@ -1,48 +1,58 @@
-
 let realTime = document.querySelector(".time-card");
 console.log(realTime);
 
 const months = [
-  "January","February","March","April","May","June",
-  "July","August","September","October","November","December"
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
 const days = [
-  "Sunday","Monday","Tuesday","Wednesday",
-  "Thursday","Friday","Saturday"
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
 ];
 
-
 //date and time
-function updateTime(){
-let date = new Date();
-let day = date.getDate();
-let dayName = days[date.getDay()]
-let currentMonth = months[date.getMonth()];
-let year = date.getFullYear();
-let hours = date.getHours();
-let minutes = date.getMinutes();
-let seconds = date.getSeconds();
+function updateTime() {
+  let date = new Date();
+  let day = date.getDate();
+  let dayName = days[date.getDay()];
+  let currentMonth = months[date.getMonth()];
+  let year = date.getFullYear();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
 
+  //am pm
+  const ampm = hours >= 12 ? "PM" : "AM";
 
-
-
-//am pm
-const ampm = hours >= 12 ? "PM" : "AM ";
-
-// 12-hour conversion
+  // 12-hour conversion
   hours = hours % 12;
-  hours = hours === 0 ? 12 : hours
+  hours = hours === 0 ? 12 : hours;
 
- // leading zero
+  // leading zero
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
 
-realTime.innerHTML = ` <p class="label">Current Time</p>
+  realTime.innerHTML = ` <p class="label">Current Time</p>
          <h2>${hours}:${minutes}:${seconds} ${ampm}</h2>
         <span>${dayName}, ${day}, ${currentMonth}, ${year}</span>`;
 }
-setInterval(updateTime,1000)
+setInterval(updateTime, 1000);
 
 //to-do list section
 
@@ -50,19 +60,30 @@ let todoInput = document.querySelector(".todoInput input");
 let todoInputAddBtn = document.querySelector(".todoInput button");
 let errorText = document.querySelector(".error-text");
 let todoList = document.querySelector(".todo-list");
+let remainingTask = document.querySelector(".todo-card .small");
+let rewardCount = document.querySelector(".reward-count");
+let overviewTotalTask = document.querySelector("#total");
+let overviewTotalTaskCompleted = document.querySelector("#done");
+let overviewTotalTaskRemaining = document.querySelector("#pending");
+
+
+
+
 
 let allTodos = JSON.parse(localStorage.getItem("PDtodos")) || [];
 
-renderTodos()
+//to render all todos
+renderTodos(allTodos);
 
-function LocalStorageSetItems(){
+//set Items im local storage
+function LocalStorageSetItems() {
   localStorage.setItem("PDtodos", JSON.stringify(allTodos));
 }
 
-todoInputAddBtn.addEventListener("click",function(){
-  let newTodo = todoInput.value.trim()
-  console.log(newTodo);
-  if(newTodo === ""){
+todoInputAddBtn.addEventListener("click", function () {
+  let newTodo = todoInput.value.trim();
+  // console.log(newTodo);
+  if (newTodo === "") {
     errorText.style.display = "block";
     return;
   }
@@ -70,18 +91,43 @@ todoInputAddBtn.addEventListener("click",function(){
   errorText.style.display = "none";
 
   let todoObj = {
-    text:newTodo,
-    Id:Date.now(),
-    isCompleted:false
-  }
-  console.log(todoObj);
+    text: newTodo,
+    id: Date.now(),
+    isCompleted: false,
+  };
+  // console.log(todoObj);
 
-  allTodos.push(todoObj)
+  allTodos.push(todoObj);
   LocalStorageSetItems();
 
+  createTodoElements(todoObj);
+  renderTodos();
 
-  createTodoElements(todoObj)
-})
+  todoInput.value = "";
+});
+function renderTodos() {
+  todoList.innerHTML = "";
+
+  allTodos.forEach((todo) => {
+    const li = createTodoElements(todo);
+    todoList.appendChild(li);
+  });
+
+  updateTaskCounter();
+}
+
+function updateTaskCounter() {
+  const remaining = allTodos.filter((t) => !t.isCompleted).length;
+  const total = allTodos.length;
+  const completedTask = allTodos.filter((t)=> t.isCompleted).length;
+  console.log(completedTask);
+  
+  remainingTask.textContent = `${remaining} of ${total} tasks remaining`;
+  rewardCount.textContent = `${completedTask}`;
+  overviewTotalTask.textContent = `${total}`;
+  overviewTotalTaskCompleted.textContent = `${completedTask}`;
+  overviewTotalTaskRemaining.textContent = `${remaining}`;
+}
 
 function createTodoElements(todoObj) {
   const li = document.createElement("li");
@@ -107,17 +153,30 @@ function createTodoElements(todoObj) {
 
   li.appendChild(label);
   li.appendChild(deleteBtn);
-  todoList.appendChild(li)
+  // todoList.appendChild(li)
+
+  deleteBtn.addEventListener("click", function () {
+    allTodos = allTodos.filter((t) => t.id !== todoObj.id);
+    LocalStorageSetItems();
+    renderTodos(allTodos);
+  updateTaskCounter();
+
+  });
+
+  checkbox.addEventListener("click", function () {
+    let targetedTodo = allTodos.find((t) => t.id === todoObj.id);
+    if (targetedTodo) {
+      targetedTodo.isCompleted = checkbox.checked;
+      li.classList.add("done");
+    }
+    li.classList.add("active");
+    LocalStorageSetItems();
+    renderTodos();
+
+  updateTaskCounter();
+
+  });
 
   return li;
 }
 
-
-function renderTodos() {
-  todoList.innerHTML = ""; // clear first
-
-  allTodos.forEach(todo => {
-    const li = createTodoElements(todo);
-    todoList.appendChild(li);
-  });
-}
